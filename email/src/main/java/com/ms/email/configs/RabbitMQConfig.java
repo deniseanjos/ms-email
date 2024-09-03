59b7835844ca32b7c2eec8b578b6d0691ba3911b
Denise Anjos <[200~deniseb.anjos@outlook.com>
2024-09-02 22:02:25 -0300

    Configuracao inicial RabbitMQ

---
+++ email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -0,0 +1,19 @@
+package com.ms.email.configs;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${spring.rabbitmq.queu}")
+    private String queue;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queue, true);
+    }
+
+}
