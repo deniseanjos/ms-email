7a2a04213507428392a2d641c7bf852213db562c
Denise Anjos <[200~deniseb.anjos@outlook.com>
2024-09-08 12:05:36 -0300

    Bean message converter e RabbitListener consumer

--- email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
+++ email/src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -1,6 +1,7 @@
 package com.ms.email.configs;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    @Value("${spring.rabbitmq.queu}")
+    @Value("${spring.rabbitmq.queue}")
     private String queue;
 
     @Bean
     Queue queue() {
         return new Queue(queue, true);
+    }
+
+    //Converter global, pode se criar especificos para cada consumer
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 
 }
